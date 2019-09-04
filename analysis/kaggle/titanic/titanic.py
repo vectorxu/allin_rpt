f9f7dcd2f3be01dd4fbcf15dc2e7653951064c67
xujian <wangshichao@allinmd.cn>
2019-09-04 09:17:48 +0800

    titanic

--- analysis/kaggle/titanic/titanic.py
+++ analysis/kaggle/titanic/titanic.py
@@ -16,6 +16,125 @@ https://mp.weixin.qq.com/s?__biz=MzUzODYwMDAzNA==&mid=2247484422&idx=1&sn=d3b243
 import pandas as pd
 import numpy as np
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#显示所有列
+pd.set_option('display.max_columns', None)
+#显示所有行
+pd.set_option('display.max_rows', None)
+#设置value的显示长度为100，默认为50
+pd.set_option('max_colwidth',100)
+
+'''
+解决中文方块问题
+'''
+plt.style.use("fivethirtyeight")
+sns.set_style({'font.sans-serif':['simhei','Arial']})
+
+plt.rcParams['font.family'] = ['Arial Unicode MS'] #解决中文显示方块问题
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 中文字体设置-黑体
+plt.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
+
+
+
 data_train = pd.read_csv('train.csv')
 data_test = pd.read_csv('test.csv')
 df = data_train.append(data_test)
+
+
+# df.info()
+# print('合并后一共{0}条数据。'.format(str(df.shape[0])))
+# print(pd.isnull(df).sum())
+
+
+
+# print(df.describe())
+
+
+
+
+
+plt.style.use("bmh")
+plt.rc('font', family='SimHei', size=13)
+cat_list = ['Pclass','Name','Sex','SibSp','Embarked','Parch','Ticket','Cabin']
+for n,i in enumerate(cat_list):
+    # 相当于group by 看有哪些枚举值，枚举有多少个
+    Cabin_cat_num = df[i].value_counts().index.shape[0]
+    print('{0}. {1}特征的类型数量是: {2}'.format(n+1,i,Cabin_cat_num))
+
+
+
+
+f, [ax1,ax2,ax3] = plt.subplots(1,3,figsize=(20,5))
+sns.countplot(x='Sex', hue='Survived', data=data_train, ax=ax1)
+sns.countplot(x='Pclass', hue='Survived', data=data_train, ax=ax2)
+sns.countplot(x='Embarked', hue='Survived', data=data_train, ax=ax3)
+ax1.set_title('Sex特征分析')
+ax2.set_title('Pclass特征分析')
+ax3.set_title('Embarked特征分析')
+f.suptitle('定类/定序数据类型特征分析',size=20,y=1.1)
+
+f, [ax1,ax2] = plt.subplots(1,2,figsize=(20,5))
+sns.countplot(x='SibSp', hue='Survived', data=data_train, ax=ax1)
+sns.countplot(x='Parch', hue='Survived', data=data_train, ax=ax2)
+ax1.set_title('SibSp特征分析')
+ax2.set_title('Parch特征分析')
+
+# plt.show()
+
+
+# 在不同社会等级下，男性和女性在不同登陆港口下的数量对比
+grid = sns.FacetGrid(df, col='Pclass', hue='Sex', palette='seismic', size=4)
+grid.map(sns.countplot, 'Embarked', alpha=0.8)
+grid.add_legend()
+
+
+# 在不同社会等级下，男性和女性在不同登陆港口下的数量对比
+grid = sns.FacetGrid(data_train, row='Sex', col='Pclass', hue='Survived', palette='seismic', size=4)
+grid.map(sns.countplot, 'Embarked', alpha=0.8)
+grid.add_legend()
+
+
+# kde分布
+f,ax = plt.subplots(figsize=(10,5))
+sns.kdeplot(data_train.loc[(data_train['Survived'] == 0),'Age'] , color='gray',shade=True,label='not survived')
+sns.kdeplot(data_train.loc[(data_train['Survived'] == 1),'Age'] , color='g',shade=True, label='survived')
+plt.title('Age特征分布 - Surviver V.S. Not Survivors', fontsize = 15)
+plt.xlabel("Age", fontsize = 15)
+plt.ylabel('Frequency', fontsize = 15)
+
+
+# 箱型图特征分析
+fig, [ax1,ax2] = plt.subplots(1,2,figsize=(20,6))
+sns.boxplot(x="Pclass", y="Age", data=data_train, ax =ax1)
+sns.swarmplot(x="Pclass", y="Age", data=data_train, ax =ax1)
+sns.kdeplot(data_train.loc[(data_train['Pclass'] == 3),'Age'] , color='b',shade=True, label='Pcalss3',ax=ax2)
+sns.kdeplot(data_train.loc[(data_train['Pclass'] == 1),'Age'] , color='g',shade=True, label='Pclass1',ax=ax2)
+sns.kdeplot(data_train.loc[(data_train['Pclass'] == 2),'Age'] , color='r',shade=True, label='Pclass2',ax=ax2)
+ax1.set_title('Age特征在Pclass下的箱型图', fontsize = 18)
+ax2.set_title("Age特征在Pclass下的kde图", fontsize = 18)
+fig.show()
+
+# Sex，Pclass分类条件下的 Age年龄对Survived的散点图
+grid = sns.FacetGrid(data_train, row='Sex', col='Pclass', hue='Survived', palette='seismic', size=3.5)
+grid.map(plt.scatter, 'PassengerId', 'Age', alpha=0.8)
+grid.add_legend()
+
+
+# 箱型图特征分析
+fig, [ax1,ax2] = plt.subplots(1,2,figsize=(20,6))
+sns.boxplot(x="Pclass", y="Fare", data=data_train, ax =ax1)
+sns.swarmplot(x="Pclass", y="Fare", data=data_train, ax =ax1)
+sns.kdeplot(data_train.loc[(data_train['Pclass'] == 3),'Fare'] , color='b',shade=True, label='Pcalss3',ax=ax2)
+sns.kdeplot(data_train.loc[(data_train['Pclass'] == 1),'Fare'] , color='g',shade=True, label='Pclass1',ax=ax2)
+sns.kdeplot(data_train.loc[(data_train['Pclass'] == 2),'Fare'] , color='r',shade=True, label='Pclass2',ax=ax2)
+ax1.set_title('Fare特征在Pclass下的箱型图', fontsize = 18)
+ax2.set_title("Fare特征在Pclass下的kde图", fontsize = 18)
+fig.show()
+
+g = sns.pairplot(data_train[[u'Survived', u'Pclass', u'Sex', u'Age', u'Parch', u'Fare', u'Embarked']], hue='Survived', palette = 'seismic',
+                 size=4,diag_kind = 'kde',diag_kws=dict(shade=True),plot_kws=dict(s=50) )
+g.set(xticklabels=[])
+
+plt.show()
